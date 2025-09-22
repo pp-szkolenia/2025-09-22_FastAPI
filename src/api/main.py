c6d114fe2a769e1f924a08ec55ca297c27f350ad
Patryk Palej <patrykpalej@gmail.com>
2025-09-22 12:20:42 +0200

    2.10 [W] Struktura projektu - podział endpointów na osobne pliki

--- src/api/main.py
+++ src/api/main.py
@@ -1,134 +1,16 @@
-from fastapi import FastAPI, HTTPException, status, Response
+from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 
-from api.models import TaskBody, UserBody
-from api.utils import get_item_by_id, get_item_index_by_id
+from api.routers import users, tasks
 
 
 app = FastAPI()
 
-
-tasks_data = [
-    {"id": 1, "description": "Learn FastAPI", "priority": 3, "is_completed": True},
-    {"id": 2, "description": "Do exercises", "priority": 2, "is_completed": False}
-]
-
-users_data = [
-    {"id": 1, "username": "Andrzej", "password": "qwerty123", "is_admin": True},
-    {"id": 2, "username": "Andżela", "password": "hasło1!", "is_admin": False},
-]
+app.include_router(tasks.router)
+app.include_router(users.router)
 
 
 @app.get("/")
 def root():
     return JSONResponse(status_code=status.HTTP_200_OK,
                         content={"message": "Hello World"})
-
-
-@app.get("/tasks")
-def get_tasks():
-    return JSONResponse(status_code=status.HTTP_200_OK,
-                        content={"result": tasks_data})
-
-
-@app.get("/tasks/{task_id}")
-def get_task_by_id(task_id: int):
-    target_task = get_item_by_id(tasks_data, task_id)
-    if not target_task:
-        message = {"error": f"Task with id {task_id} not found"}
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
-
-    return JSONResponse(status_code=status.HTTP_200_OK,
-                        content={"result": target_task})
-
-
-@app.post("/tasks", status_code=status.HTTP_201_CREATED)
-def create_task(body: TaskBody):
-    new_task: dict = body.model_dump()
-    new_task_id: int = max(task["id"] for task in tasks_data) + 1
-    new_task["id"] = new_task_id
-    tasks_data.append(new_task)
-    return {"message": "New task added", "details": new_task}
-
-
-@app.delete("/tasks/{task_id}")
-def delete_task_by_id(task_id: int):
-    target_index = get_item_index_by_id(tasks_data, task_id)
-    if target_index is None:
-        message = {"error": f"Task with id {task_id} not found"}
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=message)
-    tasks_data.pop(target_index)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-@app.put("/tasks/{task_id}")
-def update_task(task_id:int, body: TaskBody):
-    target_index = get_item_index_by_id(tasks_data, task_id)
-
-    if target_index is None:
-        message = {"error": f"Task with id {task_id} does not exist"}
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=message)
-
-    updated_task: dict = body.model_dump()
-    updated_task["id"] = task_id
-    tasks_data[target_index] = updated_task
-
-    message = {"message": f"Task with id {task_id} updated",
-               "new_value": updated_task}
-    return JSONResponse(status_code=status.HTTP_200_OK, content=message)
-
-
-@app.get("/users")
-def get_users():
-    return JSONResponse(status_code=status.HTTP_200_OK,
-                        content={"result": users_data})
-
-
-@app.get("/users/{user_id}")
-def get_user_by_id(user_id: int):
-    target_user = get_item_by_id(users_data, user_id)
-    if not target_user:
-        message = {"error": f"User with id {user_id} not found"}
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
-
-    return JSONResponse(status_code=status.HTTP_200_OK,
-                        content={"result": target_user})
-
-
-@app.post("/users", status_code=status.HTTP_201_CREATED)
-def create_user(body: UserBody):
-    new_user: dict = body.model_dump()
-    new_user_id: int = max(user["id"] for user in users_data) + 1
-    new_user["id"] = new_user_id
-    users_data.append(new_user)
-    return {"message": "New user added", "details": new_user}
-
-
-@app.delete("/users/{user_id}", include_in_schema=True)
-def delete_user_by_id(user_id: int):
-    target_index = get_item_index_by_id(users_data, user_id)
-    if target_index is None:
-        message = {"error": f"User with id {user_id} not found"}
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=message)
-    users_data.pop(target_index)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.put("/users/{user_id}")
-def update_user(user_id:int, body: UserBody):
-    target_index = get_item_index_by_id(users_data, user_id)
-
-    if target_index is None:
-        message = {"error": f"User with id {user_id} does not exist"}
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=message)
-
-    updated_user: dict = body.model_dump()
-    updated_user["id"] = user_id
-    users_data[target_index] = updated_user
-
-    message = {"message": f"User with id {user_id} updated",
-               "new_value": updated_user}
-    return JSONResponse(status_code=status.HTTP_200_OK, content=message)
