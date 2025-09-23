2fb272e7c4274486608dafea5409337f5d4c8f17
Patryk Palej <patrykpalej@gmail.com>
2025-09-23 14:57:09 +0200

    4.3 [W] Implementacji operacji bazodowanoywhc w SQLAlchemy

--- src/api/routers/users.py
+++ src/api/routers/users.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter, HTTPException, status, Response
+from fastapi import APIRouter, HTTPException, status, Response, Depends
 from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 from api.models import UserBody
 from api.utils import get_item_by_id, get_item_index_by_id
+from db.orm import get_session
+from db.models import User
 
 
 users_data = [
@@ -14,55 +18,97 @@ router = APIRouter()
 
 
 @router.get("/users")
-def get_users():
+def get_users(session: Session = Depends(get_session)):
+    with session:
+        stmt = select(User)
+        users_data = session.scalars(stmt).all()
+
+    users_data_response = [
+        {
+            "user_id": user.id_number,
+            "username": user.username,
+            "password": user.password,
+            "is_admin": user.is_admin,
+        }
+        for user in users_data
+    ]
     return JSONResponse(status_code=status.HTTP_200_OK,
-                        content={"result": users_data})
+                        content={"result": users_data_response})
 
 
 @router.get("/users/{user_id}")
-def get_user_by_id(user_id: int):
-    target_user = get_item_by_id(users_data, user_id)
+def get_user_by_id(user_id: int, session: Session = Depends(get_session)):
+    with session:
+        stmt = select(User).where(User.id_number == user_id)
+        target_user = session.scalars(stmt).first()
+
     if not target_user:
         message = {"error": f"User with id {user_id} not found"}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
 
+    response_target_user = {
+        "user_id": target_user.id_number,
+        "username": target_user.username,
+        "password": target_user.password,
+        "is_admin": target_user.is_admin,
+    }
+
     return JSONResponse(status_code=status.HTTP_200_OK,
-                        content={"result": target_user})
+                        content={"result": response_target_user})
 
 
 @router.post("/users", status_code=status.HTTP_201_CREATED)
-def create_user(body: UserBody):
-    new_user: dict = body.model_dump()
-    new_user_id: int = max(user["id"] for user in users_data) + 1
-    new_user["id"] = new_user_id
-    users_data.append(new_user)
+def create_user(body: UserBody, session: Session = Depends(get_session)):
+    new_user = User(**body.model_dump())
+    with session:
+        session.add(new_user)
+        session.commit()
+        session.refresh(new_user)
+
     return {"message": "New user added", "details": new_user}
 
 
 @router.delete("/users/{user_id}", include_in_schema=True)
-def delete_user_by_id(user_id: int):
-    target_index = get_item_index_by_id(users_data, user_id)
-    if target_index is None:
+def delete_user_by_id(user_id: int, session: Session = Depends(get_session)):
+    with session:
+        stmt = select(User).where(User.id_number==user_id)
+        target_user = session.scalars(stmt).first()
+
+    if not target_user:
         message = {"error": f"User with id {user_id} not found"}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=message)
-    users_data.pop(target_index)
+    else:
+        session.delete(target_user)
+        session.commit()
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @router.put("/users/{user_id}")
-def update_user(user_id:int, body: UserBody):
-    target_index = get_item_index_by_id(users_data, user_id)
+def update_user(user_id:int, body: UserBody, session: Session = Depends(get_session)):
+    with session:
+        stmt = select(User).where(User.id_number == user_id)
+        target_user = session.scalars(stmt).first()
 
-    if target_index is None:
-        message = {"error": f"User with id {user_id} does not exist"}
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=message)
+        if not target_user:
+            message = {"error": f"User with id {user_id} does not exist"}
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=message)
+        else:
+            update_user = target_user
+            for field, value in body.model_dump().items():
+                setattr(update_user, field, value)
+            session.commit()
+            session.refresh(update_user)
 
-    updated_user: dict = body.model_dump()
-    updated_user["id"] = user_id
-    users_data[target_index] = updated_user
+        response_update_user = {
+         "user_id": update_user.id_number,
+         "username": update_user.username,
+         "password": update_user.password,
+         "is_admin": update_user.is_admin,
+        }
 
     message = {"message": f"User with id {user_id} updated",
-               "new_value": updated_user}
+               "new_value": response_update_user}
     return JSONResponse(status_code=status.HTTP_200_OK, content=message)
